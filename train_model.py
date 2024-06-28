82236654b1b498853f8030732e77f5b23f729831
efletcher99 <ethanfletch99@gmail.com>
2024-06-27 23:09:08 -0600

    -

--- train_model.py
+++ train_model.py
@@ -1,27 +1,41 @@
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.preprocessing import StandardScaler
 import pickle
 import numpy as np
 
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-
+# Load data
 data_dict = pickle.load((open('./data.pickle', 'rb')))
-
 data = np.asarray(data_dict['data'])
 labels = np.asarray(data_dict['labels'])
 
-x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=.2, shuffle=True, stratify=labels)
+# Split data
+x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
 
-model = RandomForestClassifier()
+# Standardize data
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
 
+# Initialize model with desired parameters
+model = RandomForestClassifier(n_estimators=50, max_depth=5, min_samples_split=10, min_samples_leaf=5)
+
+# Cross-validation to evaluate model
+cv_scores = cross_val_score(model, x_train, y_train, cv=5)
+print(f"Cross-validation scores: {cv_scores}")
+print(f"Mean cross-validation score: {cv_scores.mean()}")
+
+# Train model
 model.fit(x_train, y_train)
 
+# Predict on test set
 y_predict = model.predict(x_test)
 
+# Evaluate model
 score = accuracy_score(y_predict, y_test)
-
 print('{}% of samples were classified correctly.'.format(score * 100))
 
-f = open('model.p', 'wb')
-pickle.dump({'model': model}, f)
-f.close()
+# Save model
+with open('model.p', 'wb') as f:
+    pickle.dump({'model': model}, f)

88416840a68b831947ff017dac31f1c6e77811bb
efletcher99 <ethanfletch99@gmail.com>
2024-07-09 17:19:50 -0600

    Got model to correctly recognize each letter!!

--- test_model.py
+++ test_model.py
@@ -4,6 +4,11 @@ import cv2
 import mediapipe as mp
 import numpy as np
 
+last_index = 0
+letter_count = 0
+letters_predicted = []
+current_prediction_index = 0
+
 model_dict = pickle.load(open('./model.p', 'rb'))
 model = model_dict['model']
 
@@ -12,6 +17,8 @@ cap = cv2.VideoCapture(1)
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
+
+word = ""
 
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
@@ -63,13 +70,37 @@ while True:
 
         predicted_character = labels_dict[prediction[0]]
 
+        letters_predicted.append(predicted_character)
+        if letters_predicted[current_prediction_index] == predicted_character:
+            letter_count += 1
+        else:
+            letter_count = 0
+
+        current_prediction_index += 1
+
+        if letter_count > 15:
+            word += predicted_character
+            letter_count = 0
+
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
         cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
                     cv2.LINE_AA)
 
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        font_scale = 1.3
+        thickness = 3
+        text_color = (255, 255, 255)  # White text
+        bg_color = (0, 0, 0)  # Black background
+        text_size = cv2.getTextSize(word, font, font_scale, thickness)[0]
+        text_x = (W - text_size[0]) // 2
+        text_y = H - 20  # Position 20 pixels from the bottom
+
+        cv2.rectangle(frame, (text_x - 10, text_y - text_size[1] - 10),
+                      (text_x + text_size[0] + 10, text_y + 10), bg_color, cv2.FILLED)
+        cv2.putText(frame, word, (text_x, text_y), font, font_scale, text_color, thickness, cv2.LINE_AA)
+
     cv2.imshow('frame', frame)
     cv2.waitKey(1)
-
 
 cap.release()
 cv2.destroyAllWindows()
